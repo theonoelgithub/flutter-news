a39fd52124092adaf99cb980a04db162e3db3533
Theo Noel <theo.noel59@gmail.com>
2019-03-06 11:45:10 +0100

    section 20 sqlite

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,20 +1,21 @@
 import 'package:http/http.dart' show Client;
 import 'dart:convert';
 import '../models/item_model.dart';
+import 'dart:async';
 
 final _root = 'https://hacker-news.firebaseio.com/v0';
 
 class NewsApiProvider {
   Client client = Client();
   
-  fetchTopIds () async {
+  Future<List<int>> fetchTopIds () async {
     final response = await client.get('$_root/topstories.json');
     final ids = json.decode(response.body);
     
-    return ids;
+    return ids.cast<int>(); // cast used to tell dart that the list ids is composed of integers
   }
 
-  fetchItem(int id) async {
+  Future<ItemModel> fetchItem(int id) async {
     final response = await client.get("$_root/item/$id.json");
     final parsedJson = json.decode(response.body);
     
--- test/src/resources/news_api_provider_test.dart
+++ test/src/resources/news_api_provider_test.dart
@@ -3,3 +3,26 @@ import 'dart:convert';
 import 'package:test_api/test_api.dart';
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
+
+void main() {
+  test('FetchTopIds returns a list of ids', () async {
+    //setup of test case
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
+    });
+    final ids = await newsApi.fetchTopIds();
+    expect(ids, [1, 2, 3, 4]);
+  });
+
+  test('FetchItem returns a item model', () async {
+    final newsApi = NewsApiProvider();
+    newsApi.client =MockClient((request) async {
+      final jsonMap = {'id': 123};
+      return Response(json.encode(jsonMap), 200);
+    });
+    final item = await newsApi.fetchItem(999);
+
+    expect(item.id, 123);
+  });
+}
