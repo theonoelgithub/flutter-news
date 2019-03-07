a349f116a6288a5cae675291667832a324301f1e
Theo Noel <theo.noel59@gmail.com>
2019-03-07 13:15:41 +0100

    commit after lunch

---
+++ lib/src/screens/news_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class NewsList extends StatelessWidget {
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top News'),
+      ),
+      body: Text('Show a list here'),
+    );
+  }
+}
