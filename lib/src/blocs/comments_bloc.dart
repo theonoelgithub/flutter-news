d2d6a57a93d4ca575aa7b9911e61667a2cf61735
Theo Noel <theo.noel59@gmail.com>
2019-03-14 15:35:58 +0100

    adding comments

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -4,18 +4,21 @@ import '../models/item_model.dart';
 import '../resources/repository.dart';
 
 class CommentsBloc {
-  final _commentsFetcher = PublishSubject<int>();
-  final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+  // 2 stream controllers to avoid overfetching because of the streambuilders in the widget
+  // this avoids the stream builders to call each time the transformer which is called only once.
+  final _commentsFetcher = PublishSubject<int>(); // 1st Stream Controller 
+  final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>(); // 2nd Stream controller handling the cache map
   final _repository = Repository();
 
-
-  // getters to streams
+  // getters to stream from CommentsOutput
   Observable<Map<int, Future<ItemModel>>> get itemWithComments =>
     _commentsOutput.stream;
 
-  // getters to sinks
+  // getters to sinks from CommentsFetcher
   Function(int) get fetchItemWithComments => _commentsFetcher.sink.add;
 
+  // CommentsBloc constructor to get CommentsFetcher stream, transform it with commentsTransformer
+  // then send it to commentsOutput which is used by the stream builders
   CommentsBloc() {
     _commentsFetcher.stream
       .transform(_commentsTransformer())
--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -5,10 +5,12 @@ import 'dart:async';
 
 
 class StoriesBloc {
-  final _topIds = PublishSubject<List<int>>(); //stream controller in RXdart
+  // 2 stream controllers to avoid overfetching because of the streambuilders in the widget
+  // this avoids the stream builders to call each time the transformer which is called only once.
+  final _topIds = PublishSubject<List<int>>(); // Stream controller for the topIds.
   final _repository = Repository();
-  final _itemsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
-  final _itemsFetcher = PublishSubject<int>();
+  final _itemsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>(); // 2nd stream controller
+  final _itemsFetcher = PublishSubject<int>(); // 1st stream controller in RXdart
 
   //getters to streams
   Observable<List<int>> get topIds => _topIds.stream;
@@ -18,20 +20,25 @@ class StoriesBloc {
   Function(int) get fetchItem => _itemsFetcher.sink.add; // exposing the sink to the outside world
 
   // Constructor
+  // Listen to the stream _itemsFetcher, then transform it as a cache map send to _itemsOutput stream used by the StreamBuilder in the widget NewsList
   StoriesBloc() {
     _itemsFetcher.stream.transform(_itemsTransformer()).pipe(_itemsOutput);
   }
 
   // Functions
+
+  // 1st function called in NewsList widget to add the ids from the repository to the topIds streams, used by the StreamBuilder to build the widget list
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
     _topIds.sink.add(ids);
   }
 
+  // Clear Cache when refresh cf. NewsList widget & Refresh
   clearCache() {
     return _repository.clearCache();
   }
 
+  // use ScanStreamTransformer to build & return a cache map for each id
   _itemsTransformer(){
     return ScanStreamTransformer(
       (Map<int, Future<ItemModel>> cache, int id, index) {
